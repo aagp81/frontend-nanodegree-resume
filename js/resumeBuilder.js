9f4f326e1354c8d58c749b94a8045ab1af9da387
aagp81 <aagp81@yahoo.es>
2015-01-08 00:29:36 +0800

    Deleting console.log
    Also add a jQuery .append to div main with the var funThougths

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,3 @@
 var awesomeThoughts = "I am Adolfo and i am Awesome!";
-console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("Awesome","Fun");
-console.log(funThoughts);
+$("#main").append(funThoughts);
